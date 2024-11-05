d97247cb0cf349d46b69c980dfcd2f320cb64919
Jing Chun <jakezhou2004@gmail.com>
2024-11-05 12:20:17 +0800

    update

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -69,7 +69,7 @@ export async function POST(req) {
       );
       if (user && eventType === 'user.created') {
         try {
-          await clerkClient.users.updateUserMetadata(id, {
+          (await clerkClient()).users.updateUserMetadata(id, {
             publicMetadata: {
               userMongoId: user._id,
             },
