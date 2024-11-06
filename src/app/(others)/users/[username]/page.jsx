2fe4a6348e9524b7038a616d14fe301ac4648af2
Jing Chun <jakezhou2004@gmail.com>
2024-11-06 23:04:23 +0800

    stylingggg

--- src/app/(others)/users/[username]/page.jsx
+++ src/app/(others)/users/[username]/page.jsx
@@ -25,7 +25,7 @@ export default async function UserPage({ params }) {
   return (
     <div className='max-w-screen mx-auto min-h-screen'>
       <div className='flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-light-green'>
-        <Link href={'/'} className='hover:bg-gray-100 rounded-full p-2'>
+        <Link href={'/'} className='rounded-full p-2'>
           <HiArrowLeft className='h-5 w-5' />
         </Link>
         <h2 className='sm:text-lg'>Back</h2>
