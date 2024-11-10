a6955a81e8aac171cd5d33319894387b85523d2b
Jing Chun <jakezhou2004@gmail.com>
2024-11-10 11:16:57 +0800

    styling again

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -5,7 +5,7 @@ import MiniProfile from "./MiniProfile"
 
 export default function LeftSidebar() {
   return (
-    <div className="flex flex-col items-center gap-4 ml-10 mr-10 mt-4 mx-auto">
+    <div className="flex flex-col h-full items-center gap-4 ml-10 mr-10 pt-4 pb-4 mx-auto">
         <Link href='/'>
             <Image 
             src='/logo.png'
@@ -15,7 +15,7 @@ export default function LeftSidebar() {
             className="w-48 h-48 bg-light-brown border-8 border-light-brown cursor-pointer m-1 rounded-full transition-all duration-200"
             />
         </Link>
-        <h1 className="font-bold text-4xl text-black font-serif mb-10">Fetch!</h1>
+        <h1 className="font-bold text-4xl text-black font-serif pb-4">Fetch!</h1>
         <Link href='/' className="flex items-center justify-center text-center w-48 h-9 text-white flex items-center p-3 bg-dark-brown rounded-full transition-all duration-200 gap-1 hover:brightness-75">
             <span className="font-bold">Home</span>
         </Link>
